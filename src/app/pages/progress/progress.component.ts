ba1d82b2aeeda462b8fd8b57a9f95e708ef8800a
Elisa Méndez Cáceres <emendezcaceres@gmail.com>
2022-07-28 21:07:19 +0200

    Se añadió el componente progressbar de bootstrap

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -3,14 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class ProgressComponent implements OnInit {
+  porcentaje: number = 50;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  incrementar() {
+    if (this.porcentaje < 100) {
+      this.porcentaje += 10;
+    }
   }
 
+  decrementar() {
+    if (this.porcentaje > 0) {
+      this.porcentaje -= 10;
+    }
+  }
+
+  cambiarValor(valor: number) {
+    if (
+      (valor > 0 && this.porcentaje < 100) ||
+      (valor < 0 && this.porcentaje > 0)
+    ) {
+      this.porcentaje += valor;
+    }
+  }
 }
